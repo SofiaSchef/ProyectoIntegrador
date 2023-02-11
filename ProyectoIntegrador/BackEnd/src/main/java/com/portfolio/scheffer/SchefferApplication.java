0567334ecfa7383455803a1eb1becf0640635c36
SofiaSchef <97706831+SofiaSchef@users.noreply.github.com>
2023-02-11 19:50:34 -0300

    Login

---
+++ ProyectoIntegrador/BackEnd/src/main/java/com/portfolio/scheffer/SchefferApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.scheffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchefferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchefferApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrador/BackEnd/src/test/java/com/portfolio/scheffer/SchefferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.scheffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchefferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
