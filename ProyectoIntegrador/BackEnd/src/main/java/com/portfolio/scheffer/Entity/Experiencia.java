ee6f15d3f3885c23766cb21a24ec9cc24fa28100
SofiaSchef <97706831+SofiaSchef@users.noreply.github.com>
2023-03-04 00:38:44 -0300

    Experiencia Laboral

--- ProyectoIntegrador/BackEnd/src/main/java/com/portfolio/scheffer/Entity/Experiencia.java
+++ ProyectoIntegrador/BackEnd/src/main/java/com/portfolio/scheffer/Entity/Experiencia.java
@@ -5,10 +5,53 @@
  */
 package com.portfolio.scheffer.Entity;
 
-/**
- *
- * @author sofy4
- */
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Experiencia {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String nombreE;
+    private String descriptionE;
+    
+    //Constructores
+
+    public Experiencia() {
+    }
+
+    public Experiencia(String nombreE, String descriptionE) {
+        this.nombreE = nombreE;
+        this.descriptionE = descriptionE;
+    }
+    
+    //Getters and setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreE() {
+        return nombreE;
+    }
+
+    public void setNombreE(String nombreE) {
+        this.nombreE = nombreE;
+    }
+
+    public String getDescriptionE() {
+        return descriptionE;
+    }
+
+    public void setDescriptionE(String descriptionE) {
+        this.descriptionE = descriptionE;
+    }
     
 }
--- ProyectoIntegrador/BackEnd/src/main/java/com/portfolio/scheffer/Security/MainSecurity.java
+++ ProyectoIntegrador/BackEnd/src/main/java/com/portfolio/scheffer/Security/MainSecurity.java
@@ -54,7 +54,7 @@ public class MainSecurity {
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
-                .antMatchers("/**").permitAll()
+                .antMatchers("**").permitAll()
                 .anyRequest().authenticated();
 
         http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
