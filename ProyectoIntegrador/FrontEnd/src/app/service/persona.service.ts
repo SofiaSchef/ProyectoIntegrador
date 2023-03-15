3ae2aa9e52b47e345eddf2a5ad83a430fe0e36bb
SofiaSchef <97706831+SofiaSchef@users.noreply.github.com>
2023-03-15 00:33:31 -0300

    Hard & Soft Skill

--- ProyectoIntegrador/FrontEnd/src/app/service/persona.service.ts
+++ ProyectoIntegrador/FrontEnd/src/app/service/persona.service.ts
@@ -8,11 +8,11 @@ import { persona } from '../model/persona.model';
 })
 
 export class PersonaService {
-  URL = 'http://localhost:8080/personas/';
+  pURL = 'http://localhost:8080/personas/';
 
   constructor(private http:HttpClient) { }
 
   public getPersona(): Observable<persona>{
-    return this.http.get<persona>(this.URL+ 'traer/perfil');
+    return this.http.get<persona>(this.pURL+ 'traer/perfil');
   }
 }
