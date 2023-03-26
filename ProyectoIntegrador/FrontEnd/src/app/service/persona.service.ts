71d873f2fac744bbaea28c29fb1ad5d90a177869
SofiaSchef <97706831+SofiaSchef@users.noreply.github.com>
2023-03-26 18:36:03 -0300

    cambios en la URL

--- ProyectoIntegrador/FrontEnd/src/app/service/persona.service.ts
+++ ProyectoIntegrador/FrontEnd/src/app/service/persona.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { persona } from '../model/persona.model';
 
 @Injectable({
@@ -8,11 +9,11 @@ import { persona } from '../model/persona.model';
 })
 
 export class PersonaService {
-  pURL = 'http://localhost:8080/personas/';
+  URL = environment.URL + 'personas/';
 
   constructor(private http:HttpClient) { }
 
   public getPersona(): Observable<persona>{
-    return this.http.get<persona>(this.pURL+ 'traer/perfil');
+    return this.http.get<persona>(this.URL+ 'traer/perfil');
   }
 }
